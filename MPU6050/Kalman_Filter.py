292db5bb90ba2dc29919e98cec7232c7c0969b02
KIM Min Joong <minjoong38@gmail.com>
2023-03-29 02:07:58 +0800

    Add Kalman Filter for IMU

--- MPU6050/Kalman_Filter.py
+++ MPU6050/Kalman_Filter.py
@@ -1,45 +1,47 @@
 import numpy as np
 
-class Kalman_Filter:
+
+class KalmanFilter:
     def __init__(self):
-        #Covariance of process noise
+        # Covariance of process noise
         self.Q_theta = 0.001  
         self.Q_gy_bias = 0.003  
 
-        #Covariance of measurement noise
-        self.R_angle = 0.03  
+        # Covariance of measurement noise
+        self.R_angle = 0.03
 
-        self.x = np.array([[0.0], [0.0]])  # angle, gyro bias
-        self.P = np.array([[0.0, 0.0], [0.0, 0.0]]) #priori convariance matrix
+        self.P = np.array([[0.0, 0.0], [0.0, 0.0]])  # priori covariance matrix
 
-        self.angle = 0.0 #initial angle set at 0
-        self.bias = 0.0 #initial gyro bias set at 0
+        self.angle = 0.0  # initial angle set at 0
+        self.bias = 0.0  # initial gyro bias set at 0
+
+        self.rate = 0.0  # initial angular rate set at 0
 
     def get_angle(self, angle_new, gyro_rate, dt):
-        #Priori State Calculation
+        # Priori State Calculation
         self.rate = gyro_rate - self.bias
         self.angle += dt * self.rate
 
-        #Setting up priori covariance matrix
+        # Setting up priori covariance matrix
         self.P[0][0] -= (self.P[1][0]+self.P[0][1]-self.P[1][1]*dt-self.Q_theta)*dt
         self.P[0][1] -= self.P[1][1]*dt
         self.P[1][0] -= self.P[1][1]*dt
         self.P[1][1] += self.Q_gy_bias*dt
 
-        #Calculate difference between the actual measurement and the predicted measurement based on the current state estimate
+        # Calculate difference between actual measurement and predicted measurement based on the current state estimate
         y = angle_new - self.angle
 
-        #Set innovation covariance(S) to the priori covariance(P) plus the measurement noise covariance(R)
+        # Set innovation covariance(S) to the priori covariance(P) plus the measurement noise covariance(R)
         S = self.P[0][0] + self.R_angle
         
-        #Calculate Kalman Gain, indication of how much the current state estimate is corrected by the measurement
+        # Calculate Kalman Gain, indication of how much the current state estimate is corrected by the measurement
         K = np.array([self.P[0][0]/S, self.P[1][0]/S]) 
 
-        #Update the state estimate
+        # Update the state estimate
         self.bias += K[1]*y
         self.angle += K[0]*y
 
-        #Update posteriori error covariance matrix
+        # Update posteriori error covariance matrix
         temp00 = self.P[0][0]
         temp01 = self.P[0][1]
         self.P[0][0] -= K[0]*temp00
@@ -64,14 +66,7 @@ class Kalman_Filter:
     def set_R_angle(self, R_angle):
         self.R_angle = R_angle
 
-    def get_Q_angle(self):
-        return self.Q_angle
 
-    def get_Q_gy_bias(self):
-        return self.Q_gy_bias
-
-    def get_R_angle(self):
-        return self.R_angle
 
 
 
